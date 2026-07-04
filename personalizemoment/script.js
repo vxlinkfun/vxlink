fa37bdac733f6731dd0e941a82a638809109f24f
vxlinkfun <vxlink.fun@gmail.com>
2026-07-03 21:12:31 -0300

    Update script.js

--- personalizemoment/script.js
+++ personalizemoment/script.js
@@ -4,11 +4,13 @@ const menu = document.getElementById("menu");
 if (menuBtn && menu) {
   menuBtn.addEventListener("click", () => {
     menu.classList.toggle("active");
+    menuBtn.classList.toggle("active");
   });
 
   document.querySelectorAll(".menu a").forEach((link) => {
     link.addEventListener("click", () => {
       menu.classList.remove("active");
+      menuBtn.classList.remove("active");
     });
   });
 }
